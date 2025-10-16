8b157b4091dfabd28c26746a65831e767ed67d1e
Ellet <echo.ellet@gmail.com>
2025-10-16 23:54:09 +0300

    feat: quick backport from NeoForge 1.21.1 to Forge 1.20.1

--- src/main/java/mod/epicfight/moreskillslots/EpicFightMoreSkillSlots.java
+++ src/main/java/mod/epicfight/moreskillslots/EpicFightMoreSkillSlots.java
@@ -1,20 +1,13 @@
 package mod.epicfight.moreskillslots;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
+import net.minecraftforge.fml.common.Mod;
 import yesman.epicfight.skill.SkillSlot;
 
 @Mod(EpicFightMoreSkillSlots.MOD_ID)
 public class EpicFightMoreSkillSlots {
     public static final String MOD_ID = "epicfight_moreskillslots";
-    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public EpicFightMoreSkillSlots(IEventBus modEventBus, ModContainer modContainer) {
+    public EpicFightMoreSkillSlots() {
         SkillSlot.ENUM_MANAGER.registerEnumCls(MOD_ID, MoreSkillSlots.class);
     }
 }
